6c5bd4a6d7ee05a27bcb2ca4e239f910c2428650
JoeKahora <gachau.joel@gmail.com>
2020-09-30 18:04:55 +0300

    -Cast theresponse.json to  <Album>.

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -14,6 +14,6 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
-   return this._http.get(this._albumUrl).map(response => response.json())
+   return this._http.get(this._albumUrl).map(response => <Album>response.json())
   }
 }
